263fcfedf372994da5d2e13e1623c4ec13b1e1a2
Eismunt Tatyana <po4ta6402@gmail.com>
2017-12-18 11:14:16 +0200

    Added carpatu products and product popup responsive.

--- app/js/parts/product-popup.js
+++ app/js/parts/product-popup.js
@@ -3,25 +3,47 @@
         popup = document.getElementById("product-popup"),
         productImg = document.getElementsByClassName("products-list__item-img"),
         productName = document.getElementsByClassName("products-list__item-name"),
-        body = document.getElementsByTagName("body")[0];
+        body = document.getElementsByTagName("body")[0],
+        productImgCarpatu = document.getElementsByClassName("carpatu-tours__img"),
+        productNameCarpatu = document.getElementsByClassName("carpatu-tours__title");
+
+
     if ( popup ) {
         closePopupBtn.addEventListener("click", function (e) {
             popup.classList.remove("product-popup--active");
             body.classList.remove("no-scroll");
         });
-        for (var w = 0; w < productName.length; w++) {
-            productName[w].addEventListener("click", function (e) {
-                e.preventDefault();
-                popup.classList.add("product-popup--active");
-                body.classList.add("no-scroll");
-            })
+        if (productNameCarpatu.length > 0) {
+            for (var x = 0; x < productNameCarpatu.length; x++) {
+                productNameCarpatu[x].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    popup.classList.add("product-popup--active");
+                    body.classList.add("no-scroll");
+                })
+            }
+            for (var u = 0; u < productImgCarpatu.length; u++) {
+                productImgCarpatu[u].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    popup.classList.add("product-popup--active");
+                    body.classList.add("no-scroll");
+                })
+            }
         }
-        for (var i = 0; i < productImg.length; i++) {
-            productImg[i].addEventListener("click", function (e) {
-                e.preventDefault();
-                popup.classList.add("product-popup--active");
-                body.classList.add("no-scroll");
-            })
+        else {
+            for (var w = 0; w < productName.length; w++) {
+                productName[w].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    popup.classList.add("product-popup--active");
+                    body.classList.add("no-scroll");
+                })
+            }
+            for (var i = 0; i < productImg.length; i++) {
+                productImg[i].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    popup.classList.add("product-popup--active");
+                    body.classList.add("no-scroll");
+                })
+            }
         }
     }
     // var req = getXmlHttp();
